5b943c504b1e5abdfff92b1ba9b47eae79e24a0a
Vinitha <vinithavictor007@gmail.com>
2021-04-14 16:40:23 +0530

    comments

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    //This is for creating the ground
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
